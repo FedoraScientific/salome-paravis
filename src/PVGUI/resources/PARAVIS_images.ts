7f1d15471f6042cd0ba4745d8186c31bfddc6caa
san <san@opencascade.com>
2008-11-14 14:53:29 +0000

    Re-using ParaView image files

--- src/PVGUI/resources/PARAVIS_images.ts
+++ src/PVGUI/resources/PARAVIS_images.ts
@@ -2,16 +2,8 @@
 <context>
     <name>@default</name>
     <message>
-        <source>ICON_LIGHT</source>
-        <translation>LIGHT.png</translation>
-    </message>
-    <message>
-        <source>ICON_LINE</source>
-        <translation>line_icon.png</translation>
-    </message>
-    <message>
-        <source>ICON_PARAGRAPH</source>
-        <translation>paragraph_icon.png</translation>
+        <source>ICON_PARAVIS</source>
+        <translation>pqAppIcon22.png</translation>
     </message>
 </context>
 </TS>
