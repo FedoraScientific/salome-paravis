d3d06c9e2e7ebb9f4437e8c64bd5b0aab70cbc25
Anthony Geay <anthony.geay@edf.fr>
2014-10-06 09:00:02 +0200

    Ready for 4.2

--- src/Plugins/ParaMEDCorba/vtkParaMEDCorbaSource.cxx
+++ src/Plugins/ParaMEDCorba/vtkParaMEDCorbaSource.cxx
@@ -178,6 +178,7 @@ int vtkParaMEDCorbaSource::RequestInformation(vtkInformation* request, vtkInform
       delete iorTab;
       CORBA::release(objPara);
     }
+    //myInfo->Set(CAN_HANDLE_PIECE_REQUEST(), 1);
     myInfo->Set(vtkStreamingDemandDrivenPipeline::MAXIMUM_NUMBER_OF_PIECES(),this->TotalNumberOfPieces);
   }
   return 1;
@@ -188,11 +189,10 @@ int vtkParaMEDCorbaSource::RequestData(vtkInformation* request, vtkInformationVe
   vtkInformation *outInfo=outputVector->GetInformationObject(0);
   //
   this->UpdatePiece = vtkStreamingDemandDrivenPipeline::GetUpdatePiece(outInfo);
-  this->NumberOfPieces = outInfo->Get(vtkStreamingDemandDrivenPipeline::UPDATE_NUMBER_OF_PIECES());
+  this->NumberOfPieces = vtkStreamingDemandDrivenPipeline::GetUpdateNumberOfPieces(outInfo);
   this->GhostLevel = vtkStreamingDemandDrivenPipeline::GetUpdateGhostLevel(outInfo);
   this->StartPiece=((this->UpdatePiece*this->TotalNumberOfPieces)/this->NumberOfPieces);
   this->EndPiece=(((this->UpdatePiece+1)*this->TotalNumberOfPieces)/this->NumberOfPieces);
-  std::cerr << " ### " << this->UpdatePiece << " 22-" << this->NumberOfPieces << " 33-" << this->StartPiece << "44-" << this->EndPiece << std::endl;
   vtkMultiBlockDataSet *ret0=vtkMultiBlockDataSet::SafeDownCast(outInfo->Get(vtkDataObject::DATA_OBJECT()));
   double reqTS = 0;
   if(outInfo->Has(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEP()))
